629ae2541af44e696b4ff69fb4384331ed2cb8bf
Sungwon Kim <sungwonkim@dhcp-206-87-101-126.ubcsecure.wireless.ubc.ca>
2023-07-31 18:30:30 -0700

    set up project

---
+++ my-app/src/component/article/Article.jsx
@@ -0,0 +1,11 @@
+//functional component so use "rafce"
+import React from 'react'
+import './article.css'
+
+const Article = () => {
+  return (
+    <div>Article</div>
+  )
+}
+
+export default Article
---
+++ my-app/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+
+// App component is going to be rendered inside of the div with an ID of root
+ReactDOM.render(<App />, document.getElementById('root'));
