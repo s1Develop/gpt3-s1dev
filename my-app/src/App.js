531e384bf629fabbaff5be32c345ab4f42bab341
Sungwon Kim <sungwonkim@dhcp-206-87-101-126.ubcsecure.wireless.ubc.ca>
2023-07-31 19:21:58 -0700

    until 43:29 in video

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,24 @@
 import React from 'react'
 
+// components폴더안에서 index.js파일을 만들음으로써 components폴더 안에 있는 파일들을 전부 한꺼번에 import할 수 있게 만든 것.
+import { Brand, CTA, Navbar } from './components';
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import './App.css';
+
 const App = () => {
   return (
-    <div>
-        <h1>GPT-3</h1>
+    <div className='App'>
+        <div className='gradient__bg'>
+            <Navbar />
+            <Header />
+        </div>
+        <Brand />
+        <WhatGPT3 />
+        <Features />
+        <Possibility />
+        <CTA />
+        <Blog />
+        <Footer />
     </div>
   )
 }
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
+import './index.css';
 
 
 // App component is going to be rendered inside of the div with an ID of root
 ReactDOM.render(<App />, document.getElementById('root'));
+
