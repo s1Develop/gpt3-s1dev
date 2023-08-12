57c80396a09cf26743e00c11d221f888505e7673
s1Develop <sungwon.dev@gmail.com>
2023-08-11 22:11:19 -0700

    1:08:30

--- my-app/src/containers/header/Header.jsx
+++ my-app/src/containers/header/Header.jsx
@@ -3,7 +3,15 @@ import './header.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='gpt3__header section__padding' id='home'>
+      <div className='gpt3__header-content'>
+        <h1 className='gradient__text'>
+        Let’s Build Something amazing with GPT-3 OpenAI
+        </h1>
+        
+      </div>
+      Header
+    </div>
   )
 }
 
