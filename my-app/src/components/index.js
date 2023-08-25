c2943ddd946733bd2f43ba92b20ce10f65716a5d
s1Develop <sungwon.dev@gmail.com>
2023-08-25 16:52:52 -0700

    Updates

--- my-app/src/components/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Article} from './article/Article';
-export { default as Brand} from './brand/Brand';
-export { default as CTA} from './cta/CTA';
-export { default as Feature} from './feature/Feature';
-export { default as Navbar} from './navbar/Navbar';
-
-// 대충 여기 있는 components폴더 안에 있는 여러 jsx파일들을 App.js에서 쉽게 import할 수 있게 하기위해서 필요한 파일이다.
